9e1c4824849a4d85395d4d102a2afabe3851bf95
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2020-11-16 22:23:43 +0900

    application insights

--- WebApplication12-cscore3.1/Startup.cs
+++ WebApplication12-cscore3.1/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApplication12_cscore3._1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
